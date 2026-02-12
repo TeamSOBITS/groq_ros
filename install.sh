d8fc8ca4f72fcccb4d016220001e0a290a37ada5
OnoFumiya <fumiyaono.choi@gmail.com>
2026-02-13 05:40:19 +0900

    Update new version of jazzy-devel

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
-pip3 install numpy==1.24.2
-pip3 install -q -U groq
+pip3 install numpy==1.24.2  --break-system-packages
+pip3 install -q -U groq  --break-system-packages
